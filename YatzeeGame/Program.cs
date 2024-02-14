77fcda0f5356eafa2cc3d63313c1505205690cf4
HFGF <HFGF@DESKTOP-3JBUJOU>
2024-02-14 10:59:50 +0100

    Add project files.

---
+++ YatzeeGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YatzeeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
